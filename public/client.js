5480adf05997b96d2c044a9c156b004d6a927f26
samanthayip <sty5@cornell.edu>
2018-02-12 20:27:19 -0500

    wrote changeborder function

--- public/client.js
+++ public/client.js
@@ -28,6 +28,10 @@ function ledOFF() {
 function takePicture(){
   socket.emit('takePicture');
 }
+	
+function changeColor(color){
+	document.getElementById('pictureContainer').style.border-color = color
+}
 
 //-- Addition: This function receives the new image name and applies it to html element.
 
